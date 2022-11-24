8afeaf893fd4bb2a6302f735fb641ffa3b56a68a
aknathweb <aniknwu.cse@gmail.com>
2022-11-24 11:44:36 +0600

    done footer part

---
+++ src/Pages/Shared/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './CheapPhone.png'
+
+const Footer = () => {
+    return (
+        <footer className="footer footer-center p-10 bg-gray-300 text-black rounded">
+            <div>
+                <div className="avatar">
+                    <div className="w-24 rounded-full">
+                        <img src={logo} alt='logo' />
+                    </div>
+                </div>
+                <p className="font-bold">
+                    CheapPhone <br />Providing reasonable price phone Buy and Sell Service.
+                </p>
+                <small>Copyright © 2022 - All right reserved</small>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
