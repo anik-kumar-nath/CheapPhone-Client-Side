3bc50d33456b112a46aa892b8f33136d42081a5b
aknathweb <aniknwu.cse@gmail.com>
2022-11-27 00:32:19 +0600

    perform buyer and seller delete operation

--- src/Dashboad/Admin/AllBuyers/AllBuyers.js
+++ src/Dashboad/Admin/AllBuyers/AllBuyers.js
@@ -1,11 +1,67 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import Loading from '../../../Pages/Shared/Loading';
+import toast from 'react-hot-toast';
 
 const AllBuyers = () => {
+    const { data: buyers, isLoading, refetch } = useQuery({
+        queryKey: ['allbuyer'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/allbuyer`);
+            const data = await res.json();
+            return data;
+        }
+    });
+
+    if (isLoading) {
+        return <Loading></Loading>;
+    }
+    const handleDeleteBuyer = (id) => {
+        fetch(`http://localhost:5000/seller/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    toast.success('Delete successfully.')
+                    refetch();
+                }
+            })
+    }
     return (
         <div>
-            AllBuyers
+            <div>
+                <div className="overflow-x-auto">
+                    <table className="table w-full">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                buyers.map((buyer, index) =>
+                                    <tr className="hover">
+                                        <th>{index + 1}</th>
+                                        <td>{buyer.name}
+                                        </td>
+                                        <td>{buyer.email}</td>
+                                        <td>
+                                            <button className='btn btn-error btn-sm' onClick={() => handleDeleteBuyer(buyer._id)}>Delete</button>
+                                        </td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
+
 
 export default AllBuyers;
--- src/Dashboad/Admin/AllSellers/AllSellers.js
+++ src/Dashboad/Admin/AllSellers/AllSellers.js
@@ -1,9 +1,70 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import Loading from '../../../Pages/Shared/Loading';
+import toast from 'react-hot-toast';
 
 const AllSellers = () => {
+    const { data: sellers, isLoading, refetch } = useQuery({
+        queryKey: ['allseller'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/allseller`);
+            const data = await res.json();
+            return data;
+        }
+    });
+
+    const handleUpdateStatus = (id) => {
+
+    }
+    const handleDeleteSeller = (id) => {
+        fetch(`http://localhost:5000/seller/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    toast.success('Delete successfully.')
+                    refetch();
+                }
+            })
+    }
+
+    if (isLoading) {
+        return <Loading></Loading>;
+    }
     return (
         <div>
-            AllSellers
+            <div>
+                <div className="overflow-x-auto">
+                    <table className="table w-full">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                sellers.map((seller, index) =>
+                                    <tr className="hover">
+                                        <th>{index + 1}</th>
+                                        <td>{seller.name}
+                                        </td>
+                                        <td>{seller.email}</td>
+                                        <td>{seller.status}</td>
+                                        <td>
+                                            <button className='btn btn-error btn-sm' onClick={() => handleDeleteSeller(seller._id)}>Delete</button>
+                                        </td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
