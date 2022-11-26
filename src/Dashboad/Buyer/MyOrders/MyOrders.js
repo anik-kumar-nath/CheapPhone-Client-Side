569f611c9531cd86ff1d223b83d1626ebe1a3d41
aknathweb <aniknwu.cse@gmail.com>
2022-11-26 23:36:09 +0600

    done my orders

--- src/Dashboad/Buyer/MyOrders/MyOrders.js
+++ src/Dashboad/Buyer/MyOrders/MyOrders.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { AuthContext } from '../../../Contexts/AuthProvider';
+import Loading from '../../../Pages/Shared/Loading';
 
 const MyOrders = () => {
+    const { user, loading } = useContext(AuthContext);
+    const { data: myorders, isLoading } = useQuery({
+        queryKey: ['myorder'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/myorder?email=${user.email}`);
+            const data = await res.json();
+            return data;
+        }
+    });
+    if (isLoading || loading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
-            MyOrders
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Product</th>
+                            <th>Title</th>
+                            <th>Price</th>
+                            <th>Sold</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            myorders.map((order, index) =>
+                                <tr className="hover">
+                                    <th>{index + 1}</th>
+                                    <td>
+                                        <div className="w-12 rounded-full">
+                                            <img src={order.productImage} alt='' />
+                                        </div>
+                                    </td>
+                                    <td>{order.productName}</td>
+                                    <td>{order.price}</td>
+                                    <td>
+                                        <button className='btn btn-success btn-sm'>Pay</button>
+                                    </td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
